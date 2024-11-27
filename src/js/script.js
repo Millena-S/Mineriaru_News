c646085c10ed1d46f344dfe22dd661bac3a7d84c
Millena-S <millena.silveira@escola.pr.gov.br>
2024-11-27 19:37:16 -0300

    src/js/script.js

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+const close = document.querySelector('.fecha');
+const anuncio = document.querySelector('.banuncio')
+
+close.addEventListener('click', ()=>{
+    anuncio.style.display = "none";
+})
