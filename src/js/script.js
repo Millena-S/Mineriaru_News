1b64ccc417b05ec073adee32d46c5c455def828b
Millena-S <millena.silveira@escola.pr.gov.br>
2024-12-04 09:10:01 -0300

    fghfgh

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,12 @@
 const close = document.querySelector('.fecha');
-const anuncio = document.querySelector('.banuncio')
+const anuncio = document.querySelector('.banuncio');
+const link = 'https://www.amazon.com.br/dp/B0BB3H373V?ref=cm_sw_r_ffobk_mwn_dp_RT4NAF2B9P87ZA5XKVE9&ref_=cm_sw_r_ffobk_mwn_dp_RT4NAF2B9P87ZA5XKVE9&social_share=cm_sw_r_ffobk_mwn_dp_RT4NAF2B9P87ZA5XKVE9&language=en_US&bestFormat=true';
 
-close.addEventListener('click', ()=>{
-    anuncio.style.display = "none";
-})
-anuncio.addEventListener('click', ()=>{
-    window.location.href = 'https://www.amazon.com.br/dp/B0BB3H373V?ref=cm_sw_r_ffobk_mwn_dp_RT4NAF2B9P87ZA5XKVE9&ref_=cm_sw_r_ffobk_mwn_dp_RT4NAF2B9P87ZA5XKVE9&social_share=cm_sw_r_ffobk_mwn_dp_RT4NAF2B9P87ZA5XKVE9&language=en_US&bestFormat=true';
-})
+close.addEventListener('click', () => {
+    window.open(link, '_blank'); // Abre o link em uma nova aba
+    anuncio.style.display = "none"; // Esconde o anúncio
+});
+
+anuncio.addEventListener('click', () => {
+    window.open(link, '_blank'); // Abre o link em uma nova aba
+});
