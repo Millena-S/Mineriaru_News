b32c6465b838617967ff725fe131480eb5370510
Millena-S <millena.silveira@escola.pr.gov.br>
2024-12-04 09:11:57 -0300

    dfgh

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,15 @@
 const close = document.querySelector('.fecha');
 const anuncio = document.querySelector('.banuncio');
 const link = 'https://www.amazon.com.br/dp/B0BB3H373V?ref=cm_sw_r_ffobk_mwn_dp_RT4NAF2B9P87ZA5XKVE9&ref_=cm_sw_r_ffobk_mwn_dp_RT4NAF2B9P87ZA5XKVE9&social_share=cm_sw_r_ffobk_mwn_dp_RT4NAF2B9P87ZA5XKVE9&language=en_US&bestFormat=true';
+let isClosed = false; // Variável de controle
 
 close.addEventListener('click', () => {
-    window.open(link, '_blank'); // Abre o link em uma nova aba
+    isClosed = true; // Marca como fechado
     anuncio.style.display = "none"; // Esconde o anúncio
 });
 
 anuncio.addEventListener('click', () => {
-    window.open(link, '_blank'); // Abre o link em uma nova aba
+    if (!isClosed) { // Verifica se não foi fechado
+        window.open(link, '_blank'); // Abre o link em uma nova aba
+    }
 });
