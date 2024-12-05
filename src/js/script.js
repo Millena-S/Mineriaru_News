46276d93d26bcaac7a6d02a35167962d09b9956d
Millena-S <millena.silveira@escola.pr.gov.br>
2024-12-05 14:53:50 -0300

    src/styles/style.css

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+const menu = document.querySelector('.menu');
+const menuOpener = document.querySelector('.menuOp');
 const close = document.querySelector('.fecha');
 const anuncio = document.querySelector('.banuncio');
 const link = 'https://www.amazon.com.br/dp/B0BB3H373V?ref=cm_sw_r_ffobk_mwn_dp_RT4NAF2B9P87ZA5XKVE9&ref_=cm_sw_r_ffobk_mwn_dp_RT4NAF2B9P87ZA5XKVE9&social_share=cm_sw_r_ffobk_mwn_dp_RT4NAF2B9P87ZA5XKVE9&language=en_US&bestFormat=true';
@@ -12,4 +14,8 @@ anuncio.addEventListener('click', () => {
     if (!isClosed) { // Verifica se não foi fechado
         window.open(link, '_blank'); // Abre o link em uma nova aba
     }
+});
+
+menuOpener.addEventListener('click', () =>{
+    menu.styles.display = 'flex';
 });
